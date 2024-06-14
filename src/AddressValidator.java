d74cdca398c13d98bfae2468693784e4c8a51731
emildeguzman <em.deguzm@gmail.com>
2024-06-14 01:32:49 -0700

    Initial Commit

---
+++ src/AddressValidator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;  // Import the Scanner class
+
+public class AddressValidator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    Scanner myObj = new Scanner(System.in);
+		
+		
+		
+		// Welcome to the address validator!
+		
+		
+		// Enter your street number and name:
+		// must start with a digit and be at leased three characters long
+		
+		
+		
+		// Enter the city:
+		
+		// Enter the 5-digit zip code:
+		
+		// Valid address. We can ship quickly to you!
+	}
+
+}
