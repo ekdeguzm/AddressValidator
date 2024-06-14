39f7540f017c684f064229a73ecd1c3f18a06099
emildeguzman <em.deguzm@gmail.com>
2024-06-14 02:17:41 -0700

    added address and city checking function to program

--- src/AddressValidator.java
+++ src/AddressValidator.java
@@ -4,17 +4,44 @@ public class AddressValidator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	    Scanner myObj = new Scanner(System.in);
+	    Scanner scnr = new Scanner(System.in);
 		
 		
-		
-		// Welcome to the address validator!
-		
-		
-		// Enter your street number and name:
-		// must start with a digit and be at leased three characters long
-		
-		
+	    System.out.println("Welcome to the address validator!");
+	    System.out.println("Enter your street number and name: ");
+	    
+	    String address = scnr.nextLine();
+	    
+	    System.out.println("Enter the city: ");
+	    
+	    String city = scnr.nextLine();
+	    
+	    String addressNumber = address.substring(0, address.indexOf(' '));
+	    
+	    // get rest of address after number
+	    String addressName = address.substring(addressNumber.length() + 1, address.length());
+	    
+	    boolean isNumeric = true; 
+	    boolean addressValid = true;
+	    
+	    // check if address starts with digit and is at leased three characters long
+	    if ((!Character.isDigit(addressNumber.charAt(0))) && (addressName.length() < 3)) {
+            isNumeric = false;
+            addressValid = false;
+         }
+	    else {
+            isNumeric = true;
+            addressValid = true;
+	    }
+	    
+	    // check if city is empty
+	    if (city.equals("") || city.equals(" ")) { // Empty string check
+            System.out.println("Invalid: must input city.");
+	    }
+	    else {
+	    	System.out.println("Great city!");
+	    }
+	    
 		
 		// Enter the city:
 		
